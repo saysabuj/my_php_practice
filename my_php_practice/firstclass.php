fccddd26b2dbc9e20ebfa793f8b5272937db2de0
User <User@SABUJ>
2015-06-06 13:31:49 +0600

    init php code as a begineer

---
+++ my_php_practice/firstclass.php
@@ -0,0 +1,19 @@
+
+<html>
+<body>
+<head>
+<meta charset = "UTF-8">
+</head>
+<center>
+<h1>We will learn in first class :What is PHP? And The Syntax of PHP-</h1>
+<h3>পিএইচপি একটা সার্ভার সাইড প্রোগ্রামিং ল্যাংগুয়েজ. PHP Means "Hyertext Preprosessor</h3>
+<img src="php.JPG" alt="php" >
+</center>>
+</body>
+</html>
+<?php
+
+echo "পিএইজ পি তে কোড করতে হলে আমাদের  উপরোক্ত ফরম্যাট এর মধ্যে লিখতে হবে";
+
+
+?>
